c17eca001ae661aee8b96cd8f4dcb9edf0fe84b9
Sierra-Lima <sean.liew.2022@scis.smu.edu.sg>
2024-09-20 20:06:20 +0800

    build: Removed popperJS and bootstrap dependencies
    Use the CDN instead

--- SPM-MeeRebus/src/views/CalendarView.vue
+++ SPM-MeeRebus/src/views/CalendarView.vue
@@ -1,8 +1,5 @@
 <script setup>
 import Calendar from '../components/Calendar.vue'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-import '@popperjs/core';
 </script>
 
 <template>
