b685850b7c03ade61697472b65895d1126d3c402
chester <chesterwong.2022@scis.smu.edu.sg>
2024-09-30 13:05:15 +0800

    feat: [SCRUM-3] added table for company calendar view

--- SPM-MeeRebus/src/components/CalendarHR.vue
+++ SPM-MeeRebus/src/components/CalendarHR.vue
@@ -119,6 +119,64 @@
       </div>
     </div>
   </div>
+
+  <main>
+    <Calendar :wfh-details="approvedWFHDetails" />
+
+    <div class="filter-container">
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Search by name, department, position, or reason"
+        class="filter-input-search"
+      />
+      <br />
+      <label for="start-date">Start Date: </label>
+      <input
+        v-model="startDate"
+        type="date"
+        id="start-date"
+        class="filter-input ml-2"
+      />
+
+      <label for="end-date">End Date: </label>
+      <input
+        v-model="endDate"
+        type="date"
+        id="end-date"
+        class="filter-input ml-2"
+      />
+    </div>
+
+    <div class="table-container">
+      <h2>WFH Arrangements</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Full Name</th>
+            <th>Department</th>
+            <th>Position</th>
+            <th>Reason</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(wfh, index) in filteredWFHDetails"
+            :key="wfh.arrangement_id"
+          >
+            <td>{{ new Date(wfh.date).toLocaleString() }}</td>
+            <td>
+              {{ wfh.employee.staff_fname + ' ' + wfh.employee.staff_lname }}
+            </td>
+            <td>{{ wfh.employee.dept }}</td>
+            <td>{{ wfh.employee.position }}</td>
+            <td>{{ wfh.reason || 'No Reason Provided' }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -301,6 +359,7 @@ export default {
 };
 </script>
 
+
 <style scoped>
 
 
@@ -375,4 +434,61 @@ export default {
   cursor: pointer;
   background-color: #f8f9fa;
 }
+
+/* Styling for filter input */
+.filter-container {
+  margin: 20px 0;
+  text-align: center;
+}
+
+.filter-input-search {
+  padding: 8px;
+  margin: 10px;
+  width: 50%;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.filter-input {
+  padding: 8px;
+  margin-right: 10px;
+  width: auto;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+
+/* Styling for the table */
+.table-container {
+  margin: 20px auto;
+  max-width: 90vw;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table th,
+.table td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: left;
+}
+
+.table th {
+  background-color: #f4f4f4;
+}
+
+.table td {
+  background-color: #fff;
+}
+
+.table-container h2 {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+
 </style>
